cb7f4623c8a6b631707482eccffed8180adbeb41
wushige <wuqiange@gmail.com>
2016-08-10 18:40:05 +0800

    管理后台修改

--- duobao-backend/src/main/java/team/wuxie/crowdfunding/controller/base/BaseController.java
+++ duobao-backend/src/main/java/team/wuxie/crowdfunding/controller/base/BaseController.java
@@ -1,12 +1,18 @@
 package team.wuxie.crowdfunding.controller.base;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import team.wuxie.crowdfunding.domain.CurrentUser;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * <p>
@@ -27,9 +33,28 @@ public class BaseController {
 
     /**
      * 获取当前登录用户
+     *
      * @return
      */
     public CurrentUser getCurrentUser() {
         return (CurrentUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    /**
+     * 重定向至地址 url
+     *
+     * @param url 请求地址
+     * @return
+     */
+    protected String redirectTo(String url) {
+        return "redirect:" + url;
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        dateFormat.setLenient(true);
+        //对于需要转换为Date类型的属性，使用DateEditor进行处理
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
     }
 }
