b2989122a257ae5143f35097c6f63e5da9990934
BestRumbleCN <1204165659@qq.com>
2016-12-13 23:31:40 +0800

    add announcement interface

--- duobao-api/src/main/java/team/wuxie/crowdfunding/controller/CommonRestController.java
+++ duobao-api/src/main/java/team/wuxie/crowdfunding/controller/CommonRestController.java
@@ -1,5 +1,14 @@
 package team.wuxie.crowdfunding.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
 import team.wuxie.crowdfunding.annotation.LoginSkip;
 import team.wuxie.crowdfunding.controller.base.BaseRestController;
 import team.wuxie.crowdfunding.service.AreaService;
@@ -48,4 +54,21 @@ public class CommonRestController extends BaseRestController {
 	public ApiResult getSubAreas(Long parentId) {
 		return ApiResult.getSuccess(MessageId.GENERAL_SUCCESS, areaService.findByParentId(parentId));
 	}
+	
+	@LoginSkip
+	@ApiOperation("首页公告（TEST）")
+	@RequestMapping(value = "/announcement", method = RequestMethod.GET)
+	public ApiResult announcement() {
+		Map<String,String> node = new HashMap<String,String>();
+		node.put("title", "信誉夺宝，活动大礼包！点击获取详情");
+		node.put("url", "https://www.hao123.com");
+		Map<String,String> node2 = new HashMap<String,String>();
+		node2.put("title", "信誉夺宝，官方夺宝攻略！点击获取详情");
+		node2.put("url", "https://www.hao123.com");
+		List<Map<String,String>> result = new ArrayList<Map<String,String>>();
+		result.add(node);
+		result.add(node2);
+		return ApiResult.getSuccess(MessageId.GENERAL_SUCCESS, result);
+	}
+	
 }
